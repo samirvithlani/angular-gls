6b8bb14581c6a673d8e8622be1c404b42b94a4b2
samirvithlani <samir.vithlani83955@gmail.com>
2024-03-03 11:03:47 +0530

    formHandling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { title } from 'process';
 import { EmployeeComponent } from './employee/employee.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
 import { NetflixHomeComponent } from './netflix-home/netflix-home.component';
 
 export const routes: Routes = [
@@ -8,5 +10,8 @@ export const routes: Routes = [
     },
     {
         path: "netflixhome", title: "netflixhome", component: NetflixHomeComponent
+    },
+    {
+        path:"formdemo" ,title:"formdemo",component:FormDemoComponent
     }
 ];
---
+++ src/app/form-demo/form-demo.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './form-demo.component.html',
+  styleUrl: './form-demo.component.css'
+})
+export class FormDemoComponent {
+
+  user = {
+    name:"",
+    email:"",
+    password:"",
+    contact:"",
+    gender:"",
+    hobbies:[]
+  }
+
+  onSubmit(){
+    console.log("form subbmited..",this.user);
+  }
+
+}
